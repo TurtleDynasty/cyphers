9c1dbb0fed17737fa10b16945757bad0ceb855b4
TurtleDynasty <TurtleDynasty@users.noreply.github.com>
2020-05-13 16:36:56 -0700

    create knock.py

---
+++ knock.py
@@ -0,0 +1,10 @@
+import sys
+
+# Expecting arg
+args=sys.argv
+argc=sys.argc
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+print 'Number of arguments:', argc, 'arguments.'
+print 'Argument List:', str(args)
